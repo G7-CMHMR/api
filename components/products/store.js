2db6668337c9755d432a945cf7686270bdcdf75b
LeonelBoccanfuso <punkneloficial@gmail.com>
2021-07-11 11:44:33 -0300

    routing getProducts

--- components/products/store.js
+++ components/products/store.js
@@ -1,17 +1,61 @@
 const {Product, Category, User ,Image, Seller, Promotion} = require('../../db');
 
+const simplificarProduct = function(product){
+    const res = {
+        name: product.name,
+        status: product.status,
+        price: product.price,
+        valuation: product.valuation,
+        stock: product.stock,
+        brand: product.brand,
+        description: product.description,
+        seller: product.seller.user.name,
+        images: product.images.map((image) => image.image),
+        categories: product.categories.map((category) => category.title),
+        discount: product.promotion.value,
+        delivery: product.promotion.delivery,
+
+    }
+    return res;
+}
+
 const store = {
     getAll_category: async function(category_name){
         let response = await Category.findOne(
             {
-            where: {title: category_name},
-            include: [{model: Product, includes: [{model:Image},{model:Category},{model:Promotion}]}]
+                where: {title: category_name},
+                include: [{
+                    model: Product,
+                    attributes: [ 'name','status','price','valuation','stock','brand','description'],
+                    include: [
+                        {
+                            model: Seller,
+                            attributes: ["id"],
+                            include: [{
+                                model: User,
+                                 attributes: ["name"],
+                            }]
+                        },
+                        {
+                            model:Image,
+                            attributes: ["image"],
+                        },
+                        {
+                            model:Category,
+                            attributes: ["title"],
+                        },
+                        {
+                            model:Promotion,
+                            attributes: ["value","delivery"],
+                        }
+                    ],
+                    
+                }]
             })
-        return response
+        return response.products.map((el) => simplificarProduct(el))
     },
     getAll: async function(){
         let response = await Product.findAll({
-            where: {visible: true},
             attributes: [ 'name','status','price','valuation','stock','brand','description'],
             include: [
                 {
@@ -35,8 +79,10 @@ const store = {
                     attributes: ["value","delivery"],
                 }
             ],
+            
         })
-        return response
+
+        return response.map((el) => simplificarProduct(el))
     }
 };
 
