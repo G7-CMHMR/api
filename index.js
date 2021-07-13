200e5882649c527a56eeb5b3b4438a98b7567600
LeonelBoccanfuso <punkneloficial@gmail.com>
2021-07-13 13:40:08 -0300

    arreglado carga de productos

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const port = api.port
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(port, host,async () => {
+  server.listen(port, host, () => {
     console.log(`Server is running at ${host}:${port}`);
-    await addProducts();
+    addProducts();
+
 })
 });
