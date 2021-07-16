66783130aaaafad21265a53617ab6c5bee4b47cb
Notshak <imntshk@.gmail.com>
2021-07-16 17:27:42 -0300

    se actualizo post cart/add; y get cart/:userId

--- components/auth/controller.js
+++ components/auth/controller.js
@@ -43,10 +43,6 @@ const create = async (user) => {
 
         const newUser = await User.create(user);
 
-        const cart = await Cart.create(cart);
-
-        User.addCart(cart);
-
         const isNewUserCreated = Object.keys(newUser).length > 0;
         const cart = await Cart.create();
         cart.setUser(newUser);
