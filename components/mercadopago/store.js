469b1ad3a548e18c96cc876a2b37d6c897e28f4c
Makeron <44731281+M4K3R0N@users.noreply.github.com>
2021-07-22 20:56:46 -0300

    Fix, no podes comprar si no hay stock, descuenta stock

--- components/mercadopago/store.js
+++ components/mercadopago/store.js
@@ -8,37 +8,64 @@ const store = {
 
     createPreferences: async function (params) {
 
+        console.log(params)
+
         let items = []
         let shipping = 0;
+        let error = '';
+        let errorChangeAmount = '';
+        let productid = ''
+        let newAmount = ''
 
         params.map((element) => {
 
-            if (!element.product.promotion.delivery) { shipping = 400 }
-        items.push({
-            title: element.product.name,
-            unit_price: Math.floor(((element.product.price) - ((element.product.price * element.product.promotion.value)/100)) + shipping),
-            quantity: element.amount,
+            if (!element.product.promotion.delivery) { shipping = 400 } //result.name.substring(0, maxCharactersResults)
+            if (element.product.stock < 1) { error = `No hay mas stock del producto ${element.product.name.substring(0, 24)}, tuvimos que removerlo de tu carrito`; productid = element.product.id }
+            if (element.product.stock < element.amount) {
+                errorChangeAmount = `¡No hay suficientes ${element.product.name.substring(0, 24)}, cambiamos la cantidad a ${element.product.stock} unidades!`;
+                newAmount = element.product.stock
+            }
+            if (error != '') { return { error, productid } }
+            if (errorChangeAmount != '') { return { errorChangeAmount, newAmount } }
+
+            items.push({
+                title: element.product.name,
+                unit_price: Math.floor(((element.product.price) - ((element.product.price * element.product.promotion.value) / 100)) + shipping),
+                quantity: element.amount,
+            })
         })
 
-    })
-
+        params.map(async (element) => {
+            let products = await Product.findOne({
+                where: {
+                    id : element.product.id
+                }
+            })
+            console.log(products.stock)
+            console.log(typeof(element.amount))
+            console.log(element.amount)
+            console.log(products.stock - element.amount)
+            products.stock = products.stock - element.amount
+            if(products.stock == 0){products.visible = false}
+            await products.save()
+            //console.log(products)
+        })
 
         let preference = {
-        items,
-        back_urls: {
-            "success": "http://localhost:3000/Compras",
-            "failure": "http://localhost:3000/Carrito",
-            "pending": "http://localhost:3000/Compras"
-        },
-        auto_return: 'approved',
-    };
-
+            items,
+            back_urls: {
+                "success": "http://localhost:3000/Compras",
+                "failure": "http://localhost:3000/Carrito",
+                "pending": "http://localhost:3000/Compras"
+            },
+            auto_return: 'approved',
+        };
 
 
         const res = await mercadopago.preferences.create(preference)
 
         return res;
-},
+    },
 
 };
 
