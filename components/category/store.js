b541d9bab87003f4ab46862b7d21dd085712ed47
Notshak <imntshk@.gmail.com>
2021-07-22 19:26:24 -0300

    actualice algunas rutas viejas

--- components/category/store.js
+++ components/category/store.js
@@ -16,7 +16,7 @@ const store = {
         return response.map((el) => ({ title: el.title, types: el.types.map(el => el.title)}))
     },
     postOne: async function(category_name){
-        console.log(category_name)
+        
         let response = await Category.create({title: category_name.title})
         return response
     }
