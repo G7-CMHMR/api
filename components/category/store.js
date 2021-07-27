af478a5c7dacc7aedf774fd775917fba25585004
LeonelBoccanfuso <punkneloficial@gmail.com>
2021-07-27 16:24:45 -0300

    edit categoría

--- components/category/store.js
+++ components/category/store.js
@@ -15,11 +15,23 @@ const store = {
             ]});
         return response.map((el) => ({ title: el.title, types: el.types.map(el => el.title)}))
     },
-    postOne: async function(category_name){
-        
-        let response = await Category.create({title: category_name.title})
+    postOne: async function(data){
+        let response = await Category.create({title: data.category_name})
+        data.types && data.types.map(async (type) => {
+            await this.addType({category: response.title, type_name: type})
+        })
         return response
-    }
+    },
+    addType: async function(data){
+        const current_type = await Type.create({title: data.type_name});
+        const cat = await Category.findOne({where: {title: data.category}});
+        await cat.addType(current_type);
+    },
+    editCategory: async function(data){
+        const cat = await Category.findOne({where: {title: data.category}});
+        cat.title = data.newTitle;
+        await cat.save();
+    },
 };
 
 module.exports = store;
