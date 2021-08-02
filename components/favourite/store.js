3e85be8ffd361e4763badf6b98ce39b3ac7dceb0
Notshak <imntshk@.gmail.com>
2021-08-02 17:45:29 -0300

    un fix

--- components/favourite/store.js
+++ components/favourite/store.js
@@ -2,31 +2,6 @@ const {Product, Category, Image, Promotion, Seller, User} = require('../../db');
 const {simplificarProduct} = require('../../aux_functions');
 
 const store = {
-    addFavourite: async function(params){
-        
-        const user = await User.findOne({
-            where: {id: params.userId},
-        })
-        const product = await Product.findOne({
-            where: {id: params.productId},
-        })
-
-        await user.addProduct(product);
-        return [user,product]
-    },
-    removeFavourite: async function(params){
-        const user = await User.findOne({
-            where: {id: params.userId},
-        })
-        const product = await Product.findOne({
-            where: {id: params.productId},
-        })
-        await user.removeProduct(product)
-        .catch(err => console.log(err))
-
-        return [user,product]
-        
-    },
     getFavourites: async function(userId){
         const user = await User.findOne({
             where: {id: userId},
@@ -58,6 +33,32 @@ const store = {
             }],
         })
         return user.products.map((el) => simplificarProduct(el))
+    },
+    addFavourite: async function(params){
+        
+        const user = await User.findOne({
+            where: {id: params.userId},
+        })
+        const product = await Product.findOne({
+            where: {id: params.productId},
+        })
+
+        await user.addProduct(product);
+        return await this.getFavourites(params.userId)
+        // return [user,product]
+    },
+    removeFavourite: async function(params){
+        const user = await User.findOne({
+            where: {id: params.userId},
+        })
+        const product = await Product.findOne({
+            where: {id: params.productId},
+        })
+        await user.removeProduct(product)
+        .catch(err => console.log(err))
+        return await this.getFavourites(params.userId)
+        // return [user,product]
+        
     },
 };
 
